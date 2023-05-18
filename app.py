21754d7298979e583fd563ab1d4950a43059f924
codingirlma <asmaounajaren@gmail.com>
2023-05-18 15:32:48 +0100

    add login.html, signin

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
+from register import reg
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
 
-
+app.register_blueprint(reg)
 @app.route('/')
 def index():  # put application's code here
     return render_template("index.html")
@@ -11,6 +14,9 @@ def index():  # put application's code here
 def about_page():
     return render_template("optimisation.html")
 
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
