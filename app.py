415e05101c18207728229b50e097f0c8746cd807
codingirlma <asmaounajaren@gmail.com>
2023-05-25 17:58:19 +0100

    Display pairs option in ddl--step2

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from register import reg
 import os
 import pickle
-from model import cluster_stocks,visualize_clusters
+from model import cluster_stocks,visualize_clusters,analyze_cluster_pairs
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
@@ -26,21 +26,23 @@ def index():
     if request.method == 'POST':
         selected_sector = request.form.get('sector')
         clusters = cluster_stocks(selected_sector)
-        image = visualize_clusters(selected_sector)
-        image.save(os.path.join('static', 'img', 'image.png'))
-        # Get the absolute path of the "static" directory
-        static_dir = os.path.abspath(os.path.join(os.getcwd(), 'static'))
+        plot_img=visualize_clusters(selected_sector)
+        pair_options = analyze_cluster_pairs(selected_sector, 1, 0.1, 0.1)
+        print("ALl pairs")
+        print(pair_options)
 
-        # Specify the file path using the absolute path
-        image_path = os.path.join(static_dir, 'img', image)
-
-        # Save the image
-        image.save(image_path)
-        return jsonify(clusters,image)
+        response = {
+            'clusters': clusters,
+            'plot_img': plot_img,
+            'options': pair_options
+        }
+        return jsonify(response)
     else:
-        selected_sector = 'Financials'
+        selected_sector = 'Energy'
         clusters = cluster_stocks(selected_sector)
-        return render_template('index.html', sectors=sectors, clusters=clusters)
+        plot_img=visualize_clusters(selected_sector)
+        print(plot_img)
+        return render_template('index.html', sectors=sectors, clusters=clusters,plot_img=plot_img)
 
 # print(cluster_stocks('Real_Estate'))
 @app.route("/optimisation")
--- model.py
+++ model.py
@@ -13,7 +13,7 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import statsmodels.tsa.stattools as ts
-
+import io
 #CA VA APPARAITRE EN PREMIER POUR CHOSIR UN SECTOR
 sectors = [
     'Communication_Services', 
@@ -63,7 +63,7 @@ def cluster_stocks(sector):
 
     # Cluster the stocks based on the correlation matrix using the k-means algorithm
     n_clusters = 4  # Set the number of clusters
-    kmeans = KMeans(n_clusters=n_clusters)
+    kmeans = KMeans(n_clusters=n_clusters,random_state=42)
     cluster_labels = kmeans.fit_predict(correlation_matrix)
 
     # Create a dictionary mapping stock symbols to cluster labels
@@ -78,16 +78,20 @@ def cluster_stocks(sector):
 
 #function 3 : gives the visualisation of the kmeans algo of stocks of the selected sector (it will be in the front in visualisation format)
 
+import io
+import base64
+
 def visualize_clusters(sector):
     """
     Cluster the stocks of a specific sector and visualize the clusters.
-    
+
     :param sector: A string representing the sector to cluster stocks for.
+    :return: Base64 encoded string representation of the plot image.
     """
     # Get the clusters and the price returns for the sector
     clusters_dict = cluster_stocks(sector)
+    price_returns = load_sector_data(sector)[1].pct_change().dropna()
 
-    price_returns=load_sector_data(sector)[1].pct_change().dropna()
     # Get the number of clusters
     n_clusters = len(clusters_dict)
 
@@ -99,25 +103,46 @@ def visualize_clusters(sector):
     colors = ['red', 'blue', 'green', 'orange']
     for i, cluster_label in enumerate(clusters_dict.keys()):
         cluster_stocks_list = clusters_dict[cluster_label]
-        ax.scatter(price_returns[cluster_stocks_list].mean(), price_returns[cluster_stocks_list].std(), s=point_size, color=colors[i], alpha=0.7, label=cluster_label)
+        ax.scatter(
+            price_returns[cluster_stocks_list].mean(),
+            price_returns[cluster_stocks_list].std(),
+            s=point_size,
+            color=colors[i],
+            alpha=0.7,
+            label=cluster_label
+        )
 
         # Add stock labels to each point
         for symbol in cluster_stocks_list:
-            ax.annotate(symbol, xy=(price_returns[symbol].mean(), price_returns[symbol].std()), 
-                        xytext=(5,-5), textcoords='offset points', fontsize=12)
+            ax.annotate(
+                symbol,
+                xy=(price_returns[symbol].mean(), price_returns[symbol].std()),
+                xytext=(5, -5),
+                textcoords='offset points',
+                fontsize=12
+            )
 
     ax.set_xlabel('Expected Returns', fontsize=16)
     ax.set_ylabel('Risk', fontsize=16)
-    ax.set_title(f'{n_clusters}-Cluster Solution for {sector} Sector', fontsize=20)
+    title = f'{n_clusters}-Cluster Solution for {sector} Sector'
+    ax.set_title(title, fontsize=20)
+
     ax.grid(True, linestyle='--', linewidth=0.5)
     ax.legend(fontsize=14)
-    # plt.show()
-    image_file = 'static/img/cluster_plot.png'
-    fig.savefig(image_file)
 
-    # Return the image file path
-    return image_file
+    # Save the plot image in memory
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
 
+    # Convert the plot image to a base64 encoded string
+    plot_image_string = base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+    # Close the buffer and the plot
+    buffer.close()
+    plt.close()
+
+    return plot_image_string
 
 #function 4 : saves the csv files of the clusters generated of the kmeans(it will be usefull for the next part of the trading process the results bcz we are gonna call those csv to backtest the strategies)
 
@@ -244,3 +269,127 @@ def calculate_z_score_spread(df, stock1, stock2):
     z_score_spread = (spread - spread.mean()) / spread.std()
     return z_score_spread
 
+
+# function 1 : gives the appropriate pairs selected based on cointegration and mean reversion
+
+def analyze_cluster_pairs(sector, cluster_num, significance_level_coint=0.1, significance_level_mean_revert=0.1):
+    """
+    Function to analyze stock pairs in a cluster for a given sector.
+
+    :param sector: A string representing the sector to analyze.
+    :param cluster_num: An integer representing the cluster number to analyze.
+    :param significance_level_coint: A float representing the significance level for cointegration.
+    :param significance_level_mean_revert: A float representing the significance level for mean reversion.
+    :return: A list of selected pairs for mean reversion trading.
+    """
+    # Load sector data
+    selected_stocks_df, price_data = load_sector_data(sector)
+
+    # Define the size of the train set
+    train_size = int(len(price_data) * 0.8)
+
+    # Split the data
+    train = price_data[:train_size]
+    test = price_data[train_size:]
+
+    # Cluster stocks
+    clusters_dict = cluster_stocks(sector)
+
+    # Extract the specified cluster
+    cluster = clusters_dict[f'Cluster {cluster_num}']
+
+    selected_pairs = []
+
+    # Analyze the cluster
+    n = len(cluster)
+    for i in range(n):
+        for j in range(i + 1, n):
+            stock1 = cluster[i]
+            stock2 = cluster[j]
+            # correlation = calculate_correlation(train, stock1, stock2)
+            cointegrated = is_cointegrated(train, stock1, stock2, significance_level=significance_level_coint)
+            if cointegrated:
+                residuals = calculate_residuals(train, stock1, stock2)
+                mean_reverting = is_mean_reverting(residuals, significance_level=significance_level_mean_revert)
+                if mean_reverting:
+                    selected_pairs.append((stock1, stock2))
+
+    return selected_pairs
+
+
+# ce resultat doit etre dans le doropdown de pairs pour choisir quelle pair a choisir
+# (tu peux acceder a ces pairs(le symbole seulement) par cette commandes 'selected_pairs1[i]')
+
+
+# function 2:  le PLOT de zscore and the thresholds of the chosen pairs in the previous function(the pair argument will be the  analyze_cluster_pairs(sector, 2)[0]) + trade history dataframe
+
+def plot_and_simulate_trading(pair, sector, cluster_num, upper_threshold=1, lower_threshold=-1, initial_cash=10000):
+    """
+    Function to plot trading signals for a given pair of stocks and simulate trading based on the signals.
+
+    :param pair: A tuple representing the pair of stocks.
+    :param sector: A string representing the sector to analyze.
+    :param cluster_num: An integer representing the cluster number to analyze.
+    :param upper_threshold: A float representing the upper threshold for the z-score spread.
+    :param lower_threshold: A float representing the lower threshold for the z-score spread.
+    :param initial_cash: A float representing the initial cash for trading.
+    :return: A DataFrame containing the trade history.
+    """
+    # Load sector data
+    selected_stocks_df, price_data = load_sector_data(sector)
+
+    # Define the size of the train set
+    train_size = int(len(price_data) * 0.8)
+
+    # Split the data
+    train = price_data[:train_size]
+    test = price_data[train_size:]
+
+    # The selected pair of stocks
+    stock1, stock2 = pair
+
+    # Calculate the z-score spread
+    z_score_spread = calculate_z_score_spread(test, stock1, stock2)
+
+    # Identify the buy and sell signals
+    buy_signals = z_score_spread < lower_threshold
+    sell_signals = z_score_spread > upper_threshold
+
+    # Plot trading signals
+    plt.figure(figsize=(30, 14))
+    plt.plot(z_score_spread, label='Z-score Spread', linewidth=2)
+    plt.plot(z_score_spread[buy_signals].index, z_score_spread[buy_signals], linestyle='none', marker='^',
+             color='green', label='Buy Signal', markersize=10)  # green up-triangle markers
+    plt.plot(z_score_spread[sell_signals].index, z_score_spread[sell_signals], linestyle='none', marker='v',
+             color='red', label='Sell Signal', markersize=10)  # red down-triangle markers
+    plt.axhline(0, color='black', linestyle='--', label='Mean', linewidth=2)
+    plt.axhline(upper_threshold, color='red', linestyle='--', label='Upper threshold', linewidth=2)
+    plt.axhline(lower_threshold, color='green', linestyle='--', label='Lower threshold', linewidth=2)
+    plt.title(f'Z-score Spread and Trading Signals for {stock1} and {stock2} in {sector} Sector, Cluster {cluster_num}',
+              fontsize=18)
+    plt.xlabel('Time', fontsize=14)
+    plt.ylabel('Z-score Spread', fontsize=14)
+    plt.legend(loc='best', fontsize=12)
+    plt.grid()
+    plt.show()
+
+    # Simulate trading
+    cash = initial_cash
+    position = 0  # 1 if we own the stock, -1 if we have shorted the stock
+    trades = []  # History of trades
+    for date, z_score in z_score_spread.iteritems():
+        if z_score < lower_threshold and position <= 0:
+            # Buy the stock
+            price = z_score
+            position += 1
+            cash -= price
+            trades.append({'Date': date, 'Action': 'Buy', 'Price': price, 'Cash': cash})
+        elif z_score > upper_threshold and position >= 0:
+            # Sell the stock
+            price = z_score
+            position -= 1
+            cash += price
+            trades.append({'Date': date, 'Action': 'Sell', 'Price': price, 'Cash': cash})
+
+    trades_df = pd.DataFrame(trades)
+    return trades_df
